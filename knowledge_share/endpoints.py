0b86f6e2fefcd49496d7df143f19b6785c03f783
anderson <anderson@vinta.com.br>
2017-02-20 18:39:18 -0300

    Added cookies

--- knowledge_share/endpoints.py
+++ knowledge_share/endpoints.py
@@ -1,3 +1,5 @@
+import json
+
 from django.apps import apps
 
 from rest_framework.generics import UpdateAPIView
@@ -12,3 +14,21 @@ MicroBlogPost = apps.get_model(KNOWLEDGE_APP_NAME, 'MicroBlogPost')
 class MicroBlogPostRateUpdateAPIView(UpdateAPIView):
     serializer_class = MicroBlogPostSerializer
     queryset = MicroBlogPost.objects.select_for_update().all()
+
+    def finalize_response(self, request, response, *args, **kwargs):
+        response = super(MicroBlogPostRateUpdateAPIView, self).finalize_response(
+            request, response, *args, **kwargs)
+
+        post_id = int(kwargs['pk'])
+
+        posts_id = request.COOKIES.get('posts_id')
+        if posts_id:
+            posts_id = json.loads(posts_id)
+            posts_id['posts_id'].append(post_id)
+            posts_id_json = json.dumps(posts_id)
+            response.set_cookie('posts_id', posts_id_json)
+        else:
+            posts_id_json = json.dumps({'posts_id': [post_id]})
+            response.set_cookie('posts_id', posts_id_json)
+
+        return response
