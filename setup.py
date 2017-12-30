67e0f32f0eaf0704db220cb93ae2d1074202527c
Carlos Coelho <carlos@vinta.com.br>
2017-12-30 20:35:04 -0300

    Update setup.py with missing information
    Signed-off-by: Carlos Coelho <carlos@vinta.com.br>

--- setup.py
+++ setup.py
@@ -72,6 +72,7 @@ setup(
         'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
@@ -81,5 +82,6 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
